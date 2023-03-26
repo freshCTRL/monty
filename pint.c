bff61103cadb4146e6273ccab67d7011c9a48b7d
freshCTRL <oyeboabariu25@gmail.com>
2023-03-26 09:06:02 -0700

    stackandqueue

--- pint.c
+++ pint.c
@@ -5,7 +5,7 @@ void (*f)(stack_t **stack, unsigned int line_number)
 if (*stack == NULL)
 {
 fprintf(stderr, "L%u: can't pint, stack empty\n", line_number);
-exit(1);
+exit(EXIT_FAILURE);;
 }
 else
 {
